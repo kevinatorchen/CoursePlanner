a1f3dcdc52fe79aded040458dd6de85009664c14
Myo Thiha <mthiha3@gatech.edu>
2017-07-27 18:47:29 +0630

    major ui changes

--- CourseHelper/app/src/main/java/com/example/myothiha09/coursehelper/activity/RegisterActivity.java
+++ CourseHelper/app/src/main/java/com/example/myothiha09/coursehelper/activity/RegisterActivity.java
@@ -2,18 +2,12 @@ package com.example.myothiha09.coursehelper.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.EditText;
+import butterknife.OnClick;
 import com.example.myothiha09.coursehelper.R;
-import com.example.myothiha09.coursehelper.fragment.NameFragment;
-import com.example.myothiha09.coursehelper.fragment.SchoolFragment;
 
 public class RegisterActivity extends AppCompatActivity {
-  public static SharedPreferences prefs;
 
   /**
    * A static method to start this activity more conveniently
@@ -30,5 +24,10 @@ public class RegisterActivity extends AppCompatActivity {
     setContentView(R.layout.activity_register);
   }
 
-
+  @OnClick(R.id.submit) void submit() {
+    finish();
+  }
+  @OnClick(R.id.cancel) void cancel() {
+    finish();
+  }
 }
