3741e9185d6ed981c31adb3a01bb8b1760a1eece
Myo Thiha <mthiha3@gatech.edu>
2017-07-27 19:07:42 +0630

    fixed register bug

--- CourseHelper/app/src/main/java/com/example/myothiha09/coursehelper/activity/RegisterActivity.java
+++ CourseHelper/app/src/main/java/com/example/myothiha09/coursehelper/activity/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.example.myothiha09.coursehelper.R;
 
@@ -22,6 +23,7 @@ public class RegisterActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_register);
+    ButterKnife.bind(this);
   }
 
   @OnClick(R.id.submit) void submit() {
