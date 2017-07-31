69af95edb66f142bf6c70b546b99b04a290c0443
Chen <Kevin.Chen10@T-Mobile.com>
2017-07-31 15:05:08 -0400

    Added FewerDaysOfWeekComparator

---
+++ src/FewerDaysOfWeekComparator.java
@@ -0,0 +1,9 @@
+/**
+ * Created by KChen10 on 7/31/2017.
+ */
+public class FewerDaysOfWeekComparator extends GenericComparator {
+
+    public FewerDaysOfWeekComparator(int totalGaps, int morningMinutes, int daysOfWeek, int noMealTimes, int wrongProfessors, int missingCommitments) {
+        super(0, 0, 1, 0, 100, 100);
+    }
+}
