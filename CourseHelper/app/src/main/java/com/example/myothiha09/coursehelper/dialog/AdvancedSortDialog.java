bcc9a39d2731d8a3326ee83f57acdafd51805cb3
Chen <Kevin.Chen10@T-Mobile.com>
2017-07-31 16:12:23 -0400

    Added some more SeekBars and checkboxes to Advanced Sort

--- CourseHelper/app/src/main/java/com/example/myothiha09/coursehelper/dialog/AdvancedSortDialog.java
+++ CourseHelper/app/src/main/java/com/example/myothiha09/coursehelper/dialog/AdvancedSortDialog.java
@@ -5,12 +5,16 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatDialog;
 import android.view.View;
 import android.view.Window;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.myothiha09.coursehelper.R;
+
+import org.w3c.dom.Text;
 
 import butterknife.ButterKnife;
 
@@ -21,14 +25,26 @@ import butterknife.ButterKnife;
 public class AdvancedSortDialog extends AppCompatDialog {
     private final Context context;
 
+    private final CheckBox morningClassesCheckBox;
+    private final CheckBox mealTimeCheckBox;
+
+    private final TextView morningClassesText;
+    private final TextView gapsText;
+    private final TextView daysEachWeekText;
+    private final TextView mealsText;
+    private final TextView requestedProfessorsText;
+    private final TextView requestedCommitmentsText;
+
     private final TextView morningClassesValue;
     private final TextView gapsValue;
+    private final TextView daysEachWeekValue;
     private final TextView mealsValue;
     private final TextView requestedProfessorsValue;
     private final TextView requestedCommitmentValue;
 
     private final SeekBar morningClassesSeekBar;
     private final SeekBar gapsSeekBar;
+    private final SeekBar daysEachWeekSeekBar;
     private final SeekBar mealsSeekBar;
     private final SeekBar requestedProfessorsSeekBar;
     private final SeekBar requestedCommitmentSeekBar;
@@ -43,15 +59,25 @@ public class AdvancedSortDialog extends AppCompatDialog {
 
         morningClassesValue = (TextView) findViewById(R.id.morningClassesValue);
         gapsValue = (TextView) findViewById(R.id.gapsValue);
+        daysEachWeekValue = (TextView) findViewById(R.id.daysOfWeekValue);
         mealsValue = (TextView) findViewById(R.id.mealsValue);
         requestedProfessorsValue = (TextView) findViewById(R.id.requestedProfessorsValue);
         requestedCommitmentValue = (TextView) findViewById(R.id.requestedCommitmentsValue);
 
+        morningClassesText = (TextView) findViewById(R.id.morningClassesText);
+        gapsText = (TextView) findViewById(R.id.gapsText);
+        daysEachWeekText = (TextView) findViewById(R.id.daysEachWeekText);
+        mealsText = (TextView) findViewById(R.id.mealsText);
+        requestedProfessorsText = (TextView) findViewById(R.id.requestedProfessorsText);
+        requestedCommitmentsText = (TextView) findViewById(R.id.requestedCommitmentsText);
+
+        /*
         morningClassesValue.setText(Integer.toString(-100));
         gapsValue.setText(Integer.toString(-100));
         mealsValue.setText(Integer.toString(-100));
         requestedProfessorsValue.setText(Integer.toString(0));
         requestedCommitmentValue.setText(Integer.toString(0));
+        */
 
         morningClassesSeekBar = (SeekBar) findViewById(R.id.morningClassesSeekBar);
         morningClassesSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -70,6 +96,7 @@ public class AdvancedSortDialog extends AppCompatDialog {
 
             }
         });
+        morningClassesSeekBar.setProgress(100);
         gapsSeekBar = (SeekBar) findViewById(R.id.gapsSeekBar);
         gapsSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -87,6 +114,25 @@ public class AdvancedSortDialog extends AppCompatDialog {
 
             }
         });
+        gapsSeekBar.setProgress(100);
+        daysEachWeekSeekBar = (SeekBar) findViewById(R.id.daysEachWeekSeekBar);
+        daysEachWeekSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                daysEachWeekValue.setText(Integer.toString(progress - 100));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        daysEachWeekSeekBar.setProgress(100);
         mealsSeekBar = (SeekBar) findViewById(R.id.mealTimesSeekBar);
         mealsSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -104,6 +150,7 @@ public class AdvancedSortDialog extends AppCompatDialog {
 
             }
         });
+        mealsSeekBar.setProgress(100);
         requestedProfessorsSeekBar = (SeekBar) findViewById(R.id.requestedProfessorsSeekBar);
         requestedProfessorsSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -121,6 +168,7 @@ public class AdvancedSortDialog extends AppCompatDialog {
 
             }
         });
+        requestedProfessorsSeekBar.setProgress(0);
         requestedCommitmentSeekBar = (SeekBar) findViewById(R.id.requestedCommitmentsSeekBar);
         requestedCommitmentSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -138,22 +186,30 @@ public class AdvancedSortDialog extends AppCompatDialog {
 
             }
         });
-        new MaterialDialog.Builder(getContext())
-                .positiveText("Search")
-                .onPositive(new MaterialDialog.SingleButtonCallback() {
-                    @Override
-                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
 
-                    }
-                })
-                .negativeText("Cancel")
-                .onNegative(new MaterialDialog.SingleButtonCallback() {
-                    @Override
-                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+        requestedCommitmentSeekBar.setProgress(0);
+        morningClassesCheckBox = (CheckBox) findViewById(R.id.filterMorningClasses);
+        morningClassesCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
 
-                    }
-                })
-                .show();
+                } else {
+
+                }
+            }
+        });
+        mealTimeCheckBox = (CheckBox) findViewById(R.id.mealTimeFilter);
+        mealTimeCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+
+                } else {
+
+                }
+            }
+        });
     }
 
 }
